ce31ef11fe3af365861c7ba0275f2eb8198a28ff
Bartłomiej Wojdan <wojdanek@gmail.com>
2014-05-21 20:56:37 +0200

    Made a few changes.

---
+++ ICanDoExternalTransfer/ICanDoExternalTransfer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface ICanDoExternalTransfer
+    {
+        [OperationContract]
+        bool TransferMoney(Guid clientID, Guid externalClientID, double amount);
+
+        [OperationContract]
+        Transfer[] GetPreviousTransfers();
+    }
+
+    [DataContract]
+    public class Transfer
+    {
+        [DataMember]
+        public Guid clientID { get; set; }
+        
+        [DataMember]
+        public Guid externalClientID {get; set;}
+
+        [DataMember]
+        public double amount { get; set; }
+
+        [DataMember]
+        public bool wasSuccessful { get; set; }
+
+    }
+
+}

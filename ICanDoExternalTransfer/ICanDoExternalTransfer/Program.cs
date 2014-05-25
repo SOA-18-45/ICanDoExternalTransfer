22a14c1755f0e8d1d98fbab6e7114329a8964e06
Bartłomiej Wojdan <wojdanek@gmail.com>
2014-05-25 14:45:37 +0200

    Dodano obsługę log4net oraz klasę pomocniczą LogHelper

--- ICanDoExternalTransfer/ICanDoExternalTransfer/Program.cs
+++ ICanDoExternalTransfer/ICanDoExternalTransfer/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Contracts;
 using System.ServiceModel;
 using System.ServiceModel.Description;
-using System.Runtime.Serialization;
-using Contracts;
+using log4net;
 
 namespace CanDoExternalTransfer
 {
@@ -18,13 +14,37 @@ namespace CanDoExternalTransfer
         public const string ServiceRepositoryURI = "net.tcp://localhost:11900/IServiceRepository";
     }
 
+    class LogHelper
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        public static void Debug(string logMessege)
+        {
+            log.Debug(logMessege);
+            Console.WriteLine("Debug: "+logMessege);
+        }
+
+        public static void Error(string logMessege)
+        {
+            log.Error(logMessege);
+            Console.WriteLine("Error: " + logMessege);
+        }
+
+        public static void Info(string logMessege)
+        {
+            log.Info(logMessege);
+            Console.WriteLine("Info: " + logMessege);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-
             ExternalTransfer transferMoney = new ExternalTransfer();
             ServiceHost sh = new ServiceHost(transferMoney, new Uri[] { new Uri(Constants.CanDoExternalTransferURI) });
+
+            LogHelper.Info("Application started");
 
             ServiceMetadataBehavior metadata = sh.Description.Behaviors.Find<ServiceMetadataBehavior>();
 
@@ -42,10 +62,16 @@ namespace CanDoExternalTransfer
             ChannelFactory<IServiceRepository> cf = new ChannelFactory<IServiceRepository>(new NetTcpBinding(SecurityMode.None), Constants.ServiceRepositoryURI);
             IServiceRepository serviceRepository = cf.CreateChannel();
 
-            serviceRepository.registerService(Constants.ServiceName, Constants.CanDoExternalTransferURI);
-
-            var timer = new System.Threading.Timer(e => keepConnection(serviceRepository), null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
-
+            try
+            {
+                serviceRepository.registerService(Constants.ServiceName, Constants.CanDoExternalTransferURI); 
+                var timer = new System.Threading.Timer(e => keepConnection(serviceRepository), null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error(e.Message);
+            }
+            
             Console.ReadLine();
         }
 

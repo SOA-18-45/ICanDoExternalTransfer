999debe4ef93684632cbd978fcdd8571ae1ee4d2
Bartłomiej Wojdan <wojdanek@gmail.com>
2014-05-29 04:10:12 +0200

    Dodano wersję finalną z działajacą obsługą NHibernate ORM.

---
+++ CanDoExternalTransfer/CanDoExternalTransfer/Domain/TransferItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CanDoExternalTransfer.Domain
+{
+    public class TransferItem
+    {
+        public virtual Guid ID { get; set; }
+        public virtual Guid clientID { get; set; }
+        public virtual string clientAccountNumber { get; set; }
+        public virtual string recieverAccountNumber { get; set; }
+        public virtual double amount { get; set; }
+        public virtual bool wasSuccessful { get; set; }
+        public virtual string description { get; set; }
+        public virtual DateTime date { get; set; }
+
+    }
+}
---
+++ CanDoExternalTransfer/CanDoExternalTransfer/TransferRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using Contracts;
+using CanDoExternalTransfer.Domain;
+
+namespace CanDoExternalTransfer
+{
+    class TransferRepository
+    {
+        public void Add(TransferItem newTransaction)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction dbtransaction = session.BeginTransaction())
+                {
+                    session.Save(newTransaction);
+                    dbtransaction.Commit();
+                }
+            }
+        }
+
+        public List<TransferItem> GetAllTransfersForAccount(string accountNumber)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                List<TransferItem> results = new List<TransferItem>();
+                results = (List<TransferItem>)session.QueryOver<TransferItem>().Where(x => (x.clientAccountNumber == accountNumber || x.recieverAccountNumber == accountNumber)).List<TransferItem>();
+                return results;
+            }
+        }
+
+        public List<TransferItem> GetTransfersSentFrom(string accountNumber)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                List<TransferItem> results = new List<TransferItem>();
+                results = (List<TransferItem>)session.QueryOver<TransferItem>().Where(x => x.clientAccountNumber == accountNumber).List<TransferItem>();
+                return results;
+            }
+        }
+
+        public List<TransferItem> GetTransfersSentTo(string accountNumber)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                List<TransferItem> results = new List<TransferItem>();
+                results = (List<TransferItem>)session.QueryOver<TransferItem>().Where(x => x.recieverAccountNumber == accountNumber).List<TransferItem>();
+                return results;
+            }
+        }
+
+        public List<TransferItem> GetTransfersFromDate(DateTime startDate)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                List<TransferItem> results = new List<TransferItem>();
+                results = (List<TransferItem>)session.QueryOver<TransferItem>().Where(x => x.date >= startDate).List<TransferItem>();
+                return results;
+            }
+        }
+
+        public void dropDatabase()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction dbtransaction = session.BeginTransaction())
+                {
+                    session.Delete("*");
+                    dbtransaction.Commit();
+                }
+            }
+        }
+    }
+}
